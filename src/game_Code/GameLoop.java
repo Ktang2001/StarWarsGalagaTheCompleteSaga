a9d317e3d2d910bb32cebacae3fed0885d7360f3
KalebTangen <ktangen@cub.uca.edu>
2023-11-24 12:43:41 -0600

    Added setters and getters for Player Class

---
+++ src/game_Code/GameLoop.java
@@ -0,0 +1,56 @@
+package game_Code;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameLoop extends JFrame {
+    private List<String> shipImages;
+    private Player player;
+
+    public GameLoop() {
+        shipImages = new ArrayList<>();
+       
+        shipImages.add(""); // We will need to get some images of starwars ships to put in these array indexes  
+        shipImages.add("");
+        shipImages.add("");
+
+        player = new Player(0, 0, 0, 0, null, 0, 0); 
+
+        // Set up the game loop timer
+        Timer timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform game logic here
+                updateGame();
+                repaint(); 
+            }
+        });
+        timer.start();
+    }
+
+    private void updateGame() {
+        // Update game logic here
+        // For example, move the player, check collisions, etc.
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        
+        // We will need to set up the background first
+        // Then we need to draw the player and oppents at their repective positions 
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GameLoop game = new GameLoop();
+            game.setSize(800, 600);
+            game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            game.setVisible(true);
+        });
+    }
+}
--- src/game_Code/Player.java
+++ src/game_Code/Player.java
@@ -1,5 +1,69 @@
 package game_Code;
 
+
 public class Player {
 
+
+private int lives; 
+private int speed; 
+private int xpos; 
+private int ypos; 
+private Ship playerShip ;
+private int width;
+private int height;
+
+
+public Player(int lives,int speed,int x,int y ,Ship playerShip,int width,int height) {
+ this.setLives(lives);
+ this.setSpeed(speed);
+ this.setLocaiton(x, y);
+ this.playerShip = playerShip;
+ this.width = width;
+ this.height = height;
+ }
+
+
+public int getLives() {
+ return lives;
 }
+
+public void setLives(int lives) {
+ this.lives = lives;
+}
+
+public int getSpeed() {
+ return speed;
+}
+
+public void setSpeed(int speed) {
+ this.speed = speed;
+}
+
+public int getX() {
+ return xpos;
+}
+
+public void setX(int x) {
+ this.xpos = x;
+}
+
+public int getY() {
+ return ypos;
+}
+
+public void setY(int y) {
+ this.ypos = y;
+}
+
+public void setLocaiton(int x, int y) {
+	this.ypos = y;
+	this.xpos = x;
+}
+public void setDimenstions(int width, int height) {
+	this.width = width;
+	this.height = height;
+}
+
+
+}
+
