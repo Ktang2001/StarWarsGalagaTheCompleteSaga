a2b3d0cd563dc6569ac864e8b85d6999a001265c
KalebTangen <ktangen@cub.uca.edu>
2023-12-04 21:36:55 -0600

    Update Ship

--- src/game_Code/Ship.java
+++ src/game_Code/Ship.java
@@ -10,11 +10,14 @@ public class Ship  {
 	private int numWep;
 	private String playerShip = null; 
 	private Image shipImage = null;
+	private int height = 0;
+	private int width = 0;
 	
-	public Ship(int shipNum, int numWep,String string) {
+	public Ship(int shipNum, int numWep,String string,int height,int width) {
 		this.setShipNum(shipNum);
 		this.setNumWep(numWep);
 	    this.shipImage = setImage(shipNum);
+	    this.setDimenstions(width, height);
 	}
 	public void setShipNum( int shipNum) {
 		this.shipNum = shipNum;
@@ -37,57 +40,73 @@ public class Ship  {
 	public Image setImage(int shipNum) {
 		switch(shipNum) {
 		 case 1:
-			 this.shipImage = new ImageIcon("N1_Starfigher.png").getImage();
+			 this.shipImage = new ImageIcon("N1_Starfigher.png").getImage().getScaledInstance(10,10, shipNum);
              break;
 		 case 2:
-			 this.shipImage = new ImageIcon("JediStarfighter.png").getImage();
+			 this.shipImage = new ImageIcon("JediStarfighter.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 3:
-			 this.shipImage = new ImageIcon("ARC-170.png").getImage();
+			 this.shipImage = new ImageIcon("ARC-170.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 4:
-			 this.shipImage = new ImageIcon("X-Wing.png").getImage();
+			 this.shipImage = new ImageIcon("X-Wing.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 5:
-			 this.shipImage = new ImageIcon("Y-Wing.png").getImage();
+			 this.shipImage = new ImageIcon("Y-Wing.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 6:
-			 this.shipImage = new ImageIcon("falcon.png").getImage();
+			 this.shipImage = new ImageIcon("falcon.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 7:
-			 this.shipImage = new ImageIcon("VultureDroid.png").getImage();
+			 this.shipImage = new ImageIcon("VultureDroid.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 8:
-			 this.shipImage = new ImageIcon("Tri-Fighter.png").getImage();
+			 this.shipImage = new ImageIcon("Tri-Fighter.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 9:
-			 this.shipImage = new ImageIcon("battle-station.png").getImage();
+			 this.shipImage = new ImageIcon("battle-station.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 10:
-			 this.shipImage = new ImageIcon("Grievous-Ship.png").getImage();
+			 this.shipImage = new ImageIcon("Grievous-Ship.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 11:
-			 this.shipImage = new ImageIcon("malevolence-starship.png").getImage();
+			 this.shipImage = new ImageIcon("malevolence-starship.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 12:
-			 this.shipImage = new ImageIcon("Tie-Fighter.png").getImage();
+			 this.shipImage = new ImageIcon("Tie-Fighter.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 13:
-			 this.shipImage = new ImageIcon("star-destroyer.png").getImage();
+			 this.shipImage = new ImageIcon("star-destroyer.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 14:
-			 this.shipImage = new ImageIcon("executor-star-destroyer.png").getImage();
+			 this.shipImage = new ImageIcon("executor-star-destroyer.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		 case 15:
-			 this.shipImage = new ImageIcon("death-star.png").getImage();
+			 this.shipImage = ( new ImageIcon("death-star.png").getImage().getScaledInstance(10,10, shipNum));
 			 break;
 		 case 16:
-			 this.shipImage = new ImageIcon("death-star-2.png").getImage();
+			 this.shipImage = new ImageIcon("death-star-2.png").getImage().getScaledInstance(10,10, shipNum);
 			 break;
 		}
 		return this.shipImage;
 	}
 	public Image getImage() {
 		return this.shipImage;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWidth() {
+		 return this.width;
+	}
+	public int getHeight() {
+		return this.height;
+	}
+	public void setDimenstions(int width, int height) {
+		this.width = width;
+		this.height = height;
 	}
 }
