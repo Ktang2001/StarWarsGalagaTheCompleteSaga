b9c8ed21c8fe00d723f4710e0596cf6d66ae2c2b
rileydodd <rileydodd2001@gmail.com>
2023-12-05 00:14:19 -0600

    Changes made to the projectile class

--- src/game_Code/Projectile.java
+++ src/game_Code/Projectile.java
@@ -1,65 +1,50 @@
 package game_Code;
 
-public class Projectile {
-	private String weponName;
-	private int width ;
-	private int height;
-	private int damage;
-	private int xPos;
-	private int yPos;
-	
-	
-	
-	public Projectile(int w,int h, int damage,String weponName) {
-		
-	}
-	public int getWidth () {
-		return this.width;
-	}
-	public void setWidth (int width) {
-		this.width = width ;
-	}
-	public int getHeight () {
-		return this.height;
-	}
-	public void setHeight (int height) {
-		this.height = height ;
-	}
-	public int getDamage () {
-		return this.damage;
-	}
-	public void setDamage (int damage) {
-		this.damage = damage ;
-	}
-	public void setWeponName(String weponName) {
-		this.weponName = weponName;
-	}
-	public String getWeponName(String weponName) {
-		return this.weponName;
-	}
-	public void setDimension (int width,int height) {
-		this.width = width;
-		this.height = height;		
-	}
-	public void setY(int y) {
-		this.yPos = y;
-		
-	}
-	public void setX(int x) {
-		this.xPos = x;
-	}
-	public int getY(){
-		return this.yPos;
-		
-	}
-	public int getX(){
-		return this.xPos;
-		
-	}
-	public void setLocation(int x,int y) {
-		this.xPos = x;
-		this.yPos = y;
-	}
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class Projectile {
 	
+	private int x, y;
+	private int width, height;
+	private int projectileSpeed;
+	private Color color;
+	
+	public Projectile(int x, int y, int width, int height, int projectileSpeed, Color color) {
+		this.x = x;
+		this.y = y;
+        this.width = width;
+        this.height = height;
+        this.projectileSpeed = 5;
+        this.color = color;
+	}
+	
+	public void move() {
+		y -= projectileSpeed;
+	}
+	
+	public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+	
+	public int getX() {
+        return this.x;
+    }
+	
+	public int getY() {
+        return this.y;
+    }
+	
+	public int getWidth() {
+        return this.width;
+    }
+	
+	public int getHeight() {
+        return this.height;
+    }
+	
+	public int getProjectileSpeed() {
+		return this.projectileSpeed;
+	}
 }
