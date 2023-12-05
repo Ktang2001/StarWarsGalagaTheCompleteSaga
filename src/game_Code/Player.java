6d7609cff11092b12d3aeae7898b8faf2dd5961e
rileydodd <rileydodd2001@gmail.com>
2023-12-05 01:24:22 -0600

    changes made to player

--- src/game_Code/Player.java
+++ src/game_Code/Player.java
@@ -95,6 +95,14 @@ public class Player extends JPanel implements ActionListener, KeyListener{
     public int getY() {
         return this.y;
     }
+    
+    public void setX(int x) {
+    	this.x = x;
+    }
+    
+    public void setY(int y) {
+    	this.y = y;
+    }
 
     public List<Projectile> getProjectiles() {
         return this.projectiles;
