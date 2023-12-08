bf796dca342b188a21439943c209d5d08832d857
KalebTangen <ktangen@cub.uca.edu>
2023-12-07 22:22:10 -0600

    New Projectile Class

--- src/game_Code/Projectile.java
+++ src/game_Code/Projectile.java
@@ -3,57 +3,64 @@ package game_Code;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Projectile {
-	
-	private int x, y;
-	private int projectileWidth, projectileHeight;
-	private int projectileSpeed;
-	private Color color;
-	
-	public Projectile(int x, int y, int projectileWidth, int projectileHeight, int projectileSpeed, Color color) {
-		this.x = x;
-		this.y = y;
-        this.projectileWidth = projectileWidth;
-        this.projectileHeight = projectileHeight;
-        this.projectileSpeed = 5;
-        this.color = color;
-	}
-	
-	public void move() {
-		y -= projectileSpeed;
-	}
-	
-	public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillRect(x, y, projectileWidth, projectileHeight);
-    }
-	
-	public int getX() {
-        return this.x;
-    }
-	
-	public int getY() {
-        return this.y;
-    }
-	
-	public int getWidth() {
-        return this.projectileWidth;
-    }
-	
-	public int getHeight() {
-        return this.projectileHeight;
-    }
-	
-	public int getProjectileSpeed() {
-		return this.projectileSpeed;
-	}
-	
-	public void setProjectileSpeed(int speed) {
-		this.projectileSpeed = speed;
-	}
+package Galiga_Game;
 
-	public boolean intersects(Projectile obstacleProjectile) {
-		// Need to add code for collisions!!
-		return false;
-	}
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Projectile {
+    private int x, y;
+    private int width, height;
+    private Image image;
+    private int speed = 5;
+    private boolean isPlayerProjectile;
+
+    public Projectile(int x, int y, boolean isPlayerProjectile) {
+        this.x = x;
+        this.y = y;
+        this.width = 10;
+        this.height = 20;
+        this.isPlayerProjectile = isPlayerProjectile;
+
+        if (isPlayerProjectile) {
+            this.image = new ImageIcon("blaster-bolt.png").getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        } else {
+            this.image = new ImageIcon("blaster-bolt-2.png").getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        }
+    }
+
+    public void move() {
+        if (isPlayerProjectile) {
+            y -= speed;
+        } else {
+            y += speed;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(image, x, y, null);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean isPlayerProjectile() {
+        return isPlayerProjectile;
+    }
+
 }
